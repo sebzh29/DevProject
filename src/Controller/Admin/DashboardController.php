287f69422a1b2dfbc0e7f3763b973aaa908657d8
Sébastien Glippa <sebzh29@outlook.com>
2024-03-28 11:36:43 +0100

    ajout du mapping Product dans easyadmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use App\Entity\Product;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -44,5 +45,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
         yield MenuItem::linkToCrud('Catégorie', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Produit', 'fas fa-list', Product::class);
     }
 }
---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Produit')
+            ->setEntityLabelInPlural('Produits');
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+
+            TextField::new('name')->setLabel('Nom')->setHelp('Le nom du produit.'),
+            SlugField::new('slug')->setTargetFieldName('name')->setLabel('Slug')->setHelp('Le slug du produit.'),
+            TextEditorField::new('description')->setLabel('Description')->setHelp('La description du produit.'),
+            ImageField::new('illustration')
+                ->setBasePath('/uploads/')
+                ->setLabel('Illustration')
+                ->setBasePath('/uploads')
+                ->setUploadDir('public/uploads/')
+                ->setUploadedFileNamePattern('[year]-[month]-[day]-[contenthash].[extension]')
+                ->setHelp('L\'image du produit 600x600px.'),
+            NumberField::new('price')->setLabel('Prix H.T')->setHelp('Le prix H.T du produit sans le sigle euro.'),
+            ChoiceField::new('tva')->setLabel('TVA')->setChoices([
+                '5,5%' => '5.5',
+                '10%' => '10',
+                '20%' => '20'
+            ]),
+            AssociationField::new('category')->setLabel('Catégorie associée'),
+        ];
+    }
+
+}
