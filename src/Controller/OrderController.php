240c1d3b71d9f62791b737b1d80206ce3907fc5e
Sébastien Glippa <sebzh29@outlook.com>
2024-04-02 20:14:48 +0200

    Tunnel d'achat choix adresses et transporteur + recup cde + total

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
 use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -29,11 +31,51 @@ class OrderController extends AbstractController
             null,
             [
                 'addresses' => $addresses,
+                'action' => $this->generateUrl('app_order_summary')
             ]
         );
 
         return $this->render('order/index.html.twig', [
             'deliveryForm' => $form->createView(),
+        ]);
+    }
+
+    /*
+ * 2eme étape du tunnel d'achat
+ * Récapitulatif de la commande de l'utilisateur
+ * Insertion en BDD
+ * Préparation du paiement vers Stripe
+ */
+
+    #[Route('/commande/recapitulatif', name: 'app_order_summary')]
+    public function add(
+        Request $request,
+        Cart $cart
+    ): Response
+    {
+        if ($request->getMethod() != 'POST') {
+            return $this->redirectToRoute('app_cart');
+        }
+
+
+        $form = $this->createForm(
+            OrderType::class,
+            null,
+            [
+                'addresses' => $this->getUser()->getAddresses(),
+            ]
+        );
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() || $form->isValid()) {
+           // Stocker les informations en BDD
+        }
+
+        return $this->render('order/summary.html.twig', [
+            'choices' => $form->getData(),
+            'cart' => $cart->getCart(),
+            'totalWt' => $cart->getTotalWt(),
         ]);
     }
 }
