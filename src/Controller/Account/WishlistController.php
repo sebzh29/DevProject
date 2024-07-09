df462da57462e372d4f6f24215fae5719f2fdcd9
Sébastien Glippa <sebzh29@outlook.com>
2024-07-09 11:31:32 +0200

    ajout de wishlist list, css, add et remove wish, changement couleur si wish dans la list

--- src/Controller/Account/WishlistController.php
+++ src/Controller/Account/WishlistController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Account;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,10 +17,11 @@ class WishlistController extends AbstractController
         return $this->render('account/wishlist/index.html.twig');
     }
 
-    #[Route('/compte/liste-de-souhait/ass/{id}', name: 'app_account_wishlist_add')]
+    #[Route('/compte/liste-de-souhait/add/{id}', name: 'app_account_wishlist_add')]
     public function add(
         EntityManagerInterface $em,
         ProductRepository$productRepository,
+        Request $request,
         $id
     ): Response
     {
@@ -32,5 +34,39 @@ class WishlistController extends AbstractController
             // 3. sauvegarder en bdd
             $em->flush();
         }
+
+        $this->addFlash(
+            'success',
+            'Le produit a bien été ajouté à votre liste de souhaits'
+        );
+
+        return $this->redirect($request->headers->get('referer'));
+
+    }
+
+    #[Route('/compte/liste-de-souhait/remove/{id}', name: 'app_account_wishlist_remove')]
+    public function remove(
+        EntityManagerInterface $em,
+        ProductRepository$productRepository,
+        Request $request,
+        $id
+    ): Response
+    {
+        // 1. Recuperer l'objet du produit a supprimer
+        $product = $productRepository->findOneById($id);
+
+        // 2. Si produit existant supprimer le produit de la wishlist
+        if ($product) {
+
+            $this->addFlash('success', 'Le produit a bien été supprimé de votre liste de souhaits');
+
+            $this->getUser()->removeWishlist($product);
+            // 3. sauvegarder en bdd
+            $em->flush();
+        } else {
+            $this->addFlash('danger', 'Le produit est introuvable.');
+        }
+
+        return $this->redirect($request->headers->get('referer'));
     }
 }
