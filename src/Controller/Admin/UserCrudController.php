bcc58d3d90335c9b58e4dfaad563c4afa8a82c32
Sébastien Glippa <sebzh29@outlook.com>
2024-07-10 11:49:42 +0200

    ajout securite sur le backoffice plus lien dans l espace membre test ok

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -29,6 +30,13 @@ class UserCrudController extends AbstractCrudController
         return [
             TextField::new('firstname')-> setLabel('Prénom'),
             TextField::new('lastname')-> setLabel('Nom'),
+            ChoiceField::new('roles')
+                ->setHelp('Vous pouvez choisir le rôle de votre utilisateur')
+                -> setLabel('Permissions')
+                -> setChoices([
+                'ROLE_USER' => 'ROLE_USER',
+                'ROLE_ADMIN' => 'ROLE_ADMIN',
+            ])->allowMultipleChoices(),
             TextField::new('email')-> setLabel('Email')->onlyOnIndex(),
 
         ];
